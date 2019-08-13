c653d6ffa4f7bec406b9d62ee3216e65c057e569
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2019-08-13 19:00:45 +0700

    disable crash handler for mac

--- Telegram/SourceFiles/stdafx.h
+++ Telegram/SourceFiles/stdafx.h
@@ -36,15 +36,16 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #pragma warning(pop)
 #endif // __clang__ || _MSC_VER >= 1914
 
-//CloudVeil start
-#define Q_OS_MAC
-#define TDESKTOP_DISABLE_CRASH_REPORTS
-#endif
-//CloudVeil end
 
 #ifdef OS_MAC_STORE
 #define MAC_USE_BREAKPAD
 #endif // OS_MAC_STORE
+
+//CloudVeil start
+#ifdef Q_OS_MAC
+#define TDESKTOP_DISABLE_CRASH_REPORTS
+#endif
+//CloudVeil end
 
 #include <QtWidgets/QtWidgets>
 #include <QtNetwork/QtNetwork>
@@ -98,5 +99,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_types.h"
 #include "app.h"
 #include "facades.h"
+
 
 #endif // __cplusplus
